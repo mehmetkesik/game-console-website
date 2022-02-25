54ef24e6e4a6b216183c34abf94cde709f8d4a95
mehmetkesik <livered31@gmail.com>
2022-02-25 23:41:03 +0300

    Initial Commit

---
+++ pages/games/far-cry-5.php
@@ -0,0 +1,44 @@
+<p class="tm-color-primary">About Far Cry 5</p>
+
+<p>
+    Far Cry comes to America in the latest installment of the award-winning franchise.
+    <br/><br/>
+    Welcome to Hope County, Montana, land of the free and the brave but also home to a fanatical doomsday cult known as
+    Eden’s Gate. Stand up to cult leader Joseph Seed, and his siblings, the Heralds, to spark the fires of resistance
+    and liberate the besieged community.
+</p>
+
+<p class="tm-color-primary">FIGHT AGAINST A DEADLY CULT</p>
+
+<p>
+    Free Hope County in solo or two-player co-op. Recruit Guns and Fangs for hire to help defeat the cult.
+</p>
+
+<p class="tm-color-primary">A WORLD THAT HITS BACK</p>
+
+<p>
+    Wreak havoc on the cult and its members but beware of the wrath of Joseph Seed and his followers.
+</p>
+
+<p class="tm-color-primary">CARVE YOUR OWN PATH</p>
+
+<p>
+    Build your character and choose your adventure in the largest customizable Far Cry game ever!
+</p>
+
+<p class="tm-color-primary">DYNAMIC TOYS</p>
+
+<p>
+    Take control of iconic muscle cars, ATV's, planes and a lot more to engage the cult forces in epic fights.
+</p>
+
+<p class="tm-color-primary">RE-DEFINED STEALTH MECHANICS</p>
+
+<p>
+    Enhance your gameplay with eye tracking. Tag enemies by looking at them to increase your stealth skills and help
+    your teammates spot threats.<br/>
+    Compatible with all Tobii Eye Tracking gaming devices.
+</p>
+
+<p><b>Additional notes:</b></p>
+<p>Eye tracking features available with Tobii Eye Tracking.</p>
